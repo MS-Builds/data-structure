1fb37d4e41b271aaeef52afd86068571a1122f21
MohammedSadiqOfficial <mohammedsadiqofficial.dev@gmail.com>
2026-04-05 09:51:16 +0530

    add:string

---
+++ string.js
@@ -0,0 +1,12 @@
+const s = "Hello World";
+console.log(s);
+
+// Operation
+console.log(s.concat("!")); // "Hello World" + "!"
+console.log(s.length);
+console.log(s.charAt(0))
+
+// Literals
+const name = "String";
+const greet = `Hello ${name}`;
+console.log(greet)
